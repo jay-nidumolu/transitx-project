523abd5874c63fc960e2fe7695d540bd1998aec5
Jay Nidumolu <jayakarthik2023@gmail.com>
2025-11-19 17:41:31 -0500

    Change of data path for dashboard

--- dashboard/pages/1_Transit_Analytics.py
+++ dashboard/pages/1_Transit_Analytics.py
@@ -8,7 +8,7 @@ import os
 st.title("📊 Transit Analytics — Delay Trends (TTC 2023–2024)")
 st.caption("Analyze TTC delay + weather patterns using processed data from 2023–2024.")
 
-LOCAL_PATH = "../data/processed/transit_processed.csv"
+LOCAL_PATH = "data/transit_processed.csv"
 
 # -----------------------------
 # Load Data
